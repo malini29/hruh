f1987aecf64e860ef3b702014605f367b20cf16a
malini29 <68008403+malini29@users.noreply.github.com>
2020-08-30 23:08:35 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# hruh
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,34 @@
+const Engine = Matter.Engine;
+ const World = Matter.World;
+  const Bodies = Matter.Bodies;
+   const Body = Matter.Body; 
+ 
+    var dustbinObj, paperObject,groundObject
+	 var world;
+	 function preload() {
+		paperObject=loadImage("paper.png");
+		dustbinObject=loadImage("dustbingreen.png");
+	 }
+	  function setup() {
+		   createCanvas(1600, 700);
+		    rectMode(CENTER); 
+			engine = Engine.create();
+			 world = engine.world;
+			  dustbinObj=new dustbin(1200,650);
+			   paperObject=new paper(200,450,40);
+			    groundObject=new ground(width/2,670,width,20);
+				Engine.run(engine);
+	  }
+				function draw() { 
+					rectMode(CENTER);
+					 background(0);
+					  dustbinObj.display();
+					   paperObject.display();
+					    groundObject.display();
+						 } 
+						 function keyPressed() { 
+							 if (keyCode === UP_ARROW) {
+								  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+								  } 
+								  }
+	  
